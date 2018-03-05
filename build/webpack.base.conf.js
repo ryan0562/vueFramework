99d18b457cddd6e91d7e582ab755f422d6f67338
王伟 <wangwei171@zuozh.com>
2018-03-05 16:57:27 +0800

    添加根目录'src'标识,方便import跳转

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -26,6 +26,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
+      'src': resolve('src'),
       '@': resolve('src'),
     }
   },
