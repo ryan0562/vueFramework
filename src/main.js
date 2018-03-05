d4d1e11dac61a6df3b1acba8c3ed450bb9fe44a9
王伟 <wangwei171@zuozh.com>
2018-03-05 17:07:27 +0800

    添加全局reset样式

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
+import 'src/common/reset.scss'
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +14,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
