7cb2f61bfa789bbe97d80284367c98dc39e11295
王伟 <wangwei171@zuozh.com>
2018-03-07 16:58:29 +0800

    添加mintUI demo

---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="test">
+
+  </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {};
+        },
+        created: function () {
+        },
+        components: {},
+        methods: {}
+    }
+</script>
+<style lang="scss">
+.test {
+  border: 0.5px solid #000; /*no*/
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from 'src/components/HelloWorld.vue'
+import mintIndex from 'src/pages/mintUi/demos.vue'
+import mintApp from 'src/pages/mintUi/app.vue'
+import mintDemo from 'src/pages/mintUi/route'
 
 Vue.use(Router);
-
 export default new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: () => import('src/pages/index.vue'),
+    },
+    {
+      path: '/demo',
+      component: mintIndex,
+    },
+    {
+      path: '/app',
+      component: mintApp,
+      children:mintDemo,
+    },
+    {
+      path: '*',
+      component: {
+        template: '<p>不好意思！程序员开小差了把页面弄丢了！！</p>',
+      }
     }
   ]
 })
